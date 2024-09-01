a153ea20fe537466c9d845a390158b00ad2009ce
Alexandre <kratos0500@gmail.com>
2024-09-01 18:24:06 -0300

    modificações nas rotas

--- routes/api.php
+++ routes/api.php
@@ -9,21 +9,25 @@ use Illuminate\Support\Facades\Route;
 // Rotas públicas
 Route::get('/books', [BookController::class, 'index']);
 Route::get('/books/{id}', [BookController::class, 'show']);
-Route::get('/books/search', [BookController::class, 'searchBooks']);
+Route::get('/books/search/{name}', [BookController::class, 'searchBooks']);
 Route::get('/books/{id}/download', [BookController::class, 'download']);
-Route::get('/cep', CepController::class, 'index');
+Route::get('/cep/{cep}', [CepController::class, 'index']);
+
+Route::get('/authors', [AuthorController::class, 'index']);
+Route::get('/authors/{id}', [AuthorController::class, 'show']); 
+
 
 // Rotas autenticadas
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/books', [BookController::class, 'store']);
     Route::put('/books/{id}', [BookController::class, 'update']);
+    Route::put('/authors/{id}', [AuthorController::class, 'update']);
     Route::delete('/books/{id}', [BookController::class, 'destroy']);
+    Route::delete('/authors/{id}', [AuthorController::class, 'destroy']);
     Route::post('/authors', [AuthorController::class, 'store']);
     Route::post('/logout', [AuthController::class, 'logout']);
-    
 });
 
 // Rotas de autenticação
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-
