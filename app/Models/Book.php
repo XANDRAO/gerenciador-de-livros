ecc7852e5c1ca839fa2d9d939311d964d72bb697
Alexandre <kratos0500@gmail.com>
2024-09-02 14:37:56 -0300

    modificações book.php

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,9 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
-
+    
     protected $fillable = [
-        'title', 'author_id', 'publisher', 'publication_year',
-        'pages_amount', 'isbn_number', 'file_url', 'image_name'
+        'title',
+        'author_id',
+        'publisher',
+        'publication_year',
+        'pages_amount',
+        'isbn_number',
+        'file_url',
+        'image_name',
+        'synopsis',
     ];
+    public function author()
+{
+    return $this->belongsTo(Author::class);
+}
+
 }
