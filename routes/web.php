8b7076abd6a596660b3467bc2a8aa7dbd9358d94
Alexandre <kratos0500@gmail.com>
2024-09-02 14:40:18 -0300

    tive que colocar as rotas em web.php pra continuar a aplicação

--- routes/web.php
+++ routes/web.php
@@ -6,44 +6,68 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CepController;
 use App\Http\Controllers\AuthorController;
 
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| Aqui você pode registrar as rotas da sua aplicação. Essas rotas são carregadas
+| pelo RouteServiceProvider e serão atribuídas ao grupo de middleware "web".
 |
 */
 
-Route::get('/home', function () {
+// Rota para a página inicial
+Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
-// Rotas públicas
-Route::get('/books', [BookController::class, 'index']);
-Route::get('/books/{id}', [BookController::class, 'show']);
-Route::get('/books/search/{name}', [BookController::class, 'searchBooks']);
-Route::get('/books/{id}/download', [BookController::class, 'download']);
-Route::get('/cep/{cep}', [CepController::class, 'index']);
-Route::post('/books', [BookController::class, 'store']);
+// Rotas públicas para livros
+//Route::get('/books', [BookController::class, 'index'])->name('books.index');
+Route::get('/books/search', [BookController::class, 'searchBooks'])->name('books.search');
+//Route::get('/books/{id}', [BookController::class, 'show'])->name('books.show');
+//Route::get('/books/{book}', [BookController::class, 'show'])->name('books.show');
+//Route::get('/books/{book}/edit', [BookController::class, 'edit'])->name('books.edit');
+//Route::post('/books', [BookController::class, 'store'])->name('books.store');
+////Route::put('/books/{book}', [BookController::class, 'update'])->name('books.update');
+//Route::delete('/books/{book}', [BookController::class, 'destroy'])->name('books.destroy');
+//Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
+//Route::post('/books', [BookController::class, 'store'])->name('books.store');
 
-Route::get('/authors', [AuthorController::class, 'index']);
-Route::get('/authors/{id}', [AuthorController::class, 'show']); 
+
+Route::post('/login', [AuthController::class, 'login'])->name('login.post');
 
 
-// Rotas autenticadas
+Route::resource('authors', AuthorController::class);
+Route::resource('books', BookController::class);
+
+
+//Route::get('/books/{id}/download', [BookController::class, 'download'])->name('books.download');
+
+// Rotas públicas para autores
+//Route::get('/authors', [AuthorController::class, 'index'])->name('authors.index');
+//Route::get('/authors/{id}', [AuthorController::class, 'show'])->name('authors.show');
+
+// Rota para consultar CEP
+Route::get('/cep/{cep}', [CepController::class, 'index'])->name('cep.index');
+
+// Rotas autenticadas para livros
+//Route::middleware('auth:sanctum')->group(function () {
+    //Route::put('/books/{id}', [BookController::class, 'update'])->name('books.update');
+    //Route::delete('/books/{id}', [BookController::class, 'destroy'])->name('books.destroy');
+//});
+
+// Rotas autenticadas para autores
 Route::middleware('auth:sanctum')->group(function () {
-    Route::put('/books/{id}', [BookController::class, 'update']);
-    Route::put('/authors/{id}', [AuthorController::class, 'update']);
-    Route::delete('/books/{id}', [BookController::class, 'destroy']);
-    Route::delete('/authors/{id}', [AuthorController::class, 'destroy']);
-    Route::post('/authors', [AuthorController::class, 'store']);
-    Route::post('/logout', [AuthController::class, 'logout']);
+   // Route::put('/authors/{id}', [AuthorController::class, 'update'])->name('authors.update');
+    //Route::delete('/authors/{id}', [AuthorController::class, 'destroy'])->name('authors.destroy');
+    //Route::post('/authors', [AuthorController::class, 'store'])->name('authors.store');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
 
 // Rotas de autenticação
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::get('login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('register', [AuthController::class, 'showRegistrationForm'])->name('register');
+Route::post('register', [AuthController::class, 'register'])->name('register');
+
