ece4d42eb3e4cae3f2c369f45983aa24a2febc59
Alexandre <kratos0500@gmail.com>
2024-09-03 17:13:30 -0300

    modificação nas rotas

--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,7 @@ Route::resource('books', BookController::class);
 
 // Rota para consultar CEP
 Route::get('/cep/{cep}', [CepController::class, 'index'])->name('cep.index');
+
 // Rotas autenticadas para livros
 //Route::middleware('auth:sanctum')->group(function () {
     //Route::put('/books/{id}', [BookController::class, 'update'])->name('books.update');
