38e92e371f8722db86d752d2906c30659c0a53ed
Alexandre <kratos0500@gmail.com>
2024-09-02 23:09:44 -0300

    modificações no Book

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -10,16 +10,17 @@ class Book extends Model
     use HasFactory;
     
     protected $fillable = [
-        'title',
-        'author_id',
-        'publisher',
-        'publication_year',
-        'pages_amount',
-        'isbn_number',
-        'file_url',
-        'image_name',
+        'title', 
+        'author_id', 
+        'publisher', 
+        'publication_year', 
+        'pages_amount', 
+        'isbn_number', 
+        'file_url', 
+        'image_name', 
         'synopsis',
     ];
+    
     public function author()
 {
     return $this->belongsTo(Author::class);
